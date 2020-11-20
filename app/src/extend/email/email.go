c2cfb51eb22356126d555d35466d8a92e36ab1d5
azhuang <azhuang321@gmail.com>
2020-11-20 10:30:47 +0800

    发送邮件逻辑

---
+++ app/src/extend/email/email.go
@@ -0,0 +1,37 @@
+package email
+
+import (
+	"monitor/extend/conf"
+	"net/smtp"
+	"strconv"
+)
+
+// SendEmail 发送邮件
+func SendEmail(subject string, recvEmail string, emailContent string) error {
+
+	auth := smtp.PlainAuth(
+		"",
+		conf.EmailConf.UserName,
+		conf.EmailConf.Password,
+		conf.EmailConf.Host,
+	)
+
+	msg := []byte(
+		"To: " + recvEmail + "\r\n" +
+			"From: " + conf.EmailConf.ServName + "<" + conf.EmailConf.UserName + ">\r\n" +
+			"Subject: " + subject + "\r\n" + "MIME-version: 1.0;\nContent-Type: " +
+			conf.EmailConf.ContentTypeHTML + ";charset=\"UTF-8\";\t\n\r\n" + emailContent,
+	)
+
+	err := smtp.SendMail(
+		conf.EmailConf.Host+":"+strconv.Itoa(conf.EmailConf.Port),
+		auth,
+		conf.EmailConf.UserName,
+		[]string{recvEmail},
+		msg,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
